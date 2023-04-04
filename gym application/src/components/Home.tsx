5a1746816c136c603ac73689d4ff44f773362eb1
Vladimir Orlov <satancrew.true@gmail.com>
2023-04-04 15:47:03 +0300

    feat: add some styles for Home

--- gym application/src/components/Home.tsx
+++ gym application/src/components/Home.tsx
@@ -8,6 +8,7 @@ import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 
 import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
@@ -18,13 +19,13 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="bg-gray-20 md:h-full md:pb-0 gap-16 py-10">
       {/* image and main header */}
-      <div>
+      <div className="md:flex md:h-5/6 items-center justify-center w-5/6 mx-auto">
         {/* main header */}
-        <div>
+        <div className="md:basis-3/5 z-10 mt-32">
           {/* heading */}
-          <div>
-            <div>
-              <div>
+          <div className="md:-mt-20">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] md:before:content-evolvetext">
                 <img alt="home-page-text" src={HomePageText} />
               </div>
             </div>
@@ -34,9 +35,37 @@ const Home = ({ setSelectedPage }: Props) => {
               Body Now.
             </p>
           </div>
+          {/* Actions */}
+          <div>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-primary-500 hover:text-secondary-500 text-sm font-bold underline"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
         </div>
-        <div>{/* image */}</div>
+        {/* image */}
+        <div>
+          <img alt="home-pageGraphic" src={HomePageGraphic} />
+        </div>
       </div>
+      {/* Sponsors */}
+      {isAboveMediumScreens && (
+        <div>
+          <div>
+            <div>
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
+              <img alt="forbes-sponsor" src={SponsorForbes} />
+              <img alt="fortune-sponsor" src={SponsorFortune} />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
